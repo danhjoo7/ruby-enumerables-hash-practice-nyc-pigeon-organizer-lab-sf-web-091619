f3d45f42efcd79b69b7de6bb999385079b24aa9b
Daniel Joo <danhjoo@gmail.com>
2019-09-14 17:52:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,15 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  data.each do |key, value|
-    value.each do |new_value, all_names|
+  data.each do |color_gender_lives, value|
+    value.each do |stats, all_names|
       all_names.each do |name|
         if pigeon_list[name] == nil
           pigeon_list[name] = {}
         end
-        if pigeon_list[name][key] == nil
-          pigeon_list[name][key] = []
+        if pigeon_list[name][color_gender_lives] == nil
+          pigeon_list[name][color_gender_lives] = []
         end
-        pigeon_list[name][key].push(stats.to_s)
+        pigeon_list[name][color_gender_lives].push(stats.to_s)
       end
     end 
   end
